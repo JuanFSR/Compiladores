44780ef94c8c211079da72a05cd177a71c40635d
JuanFSR <juanfrangel15@gmail.com>
2022-03-12 17:17:20 -0300

    main: Corrigindo regex para extração de emails

--- ExpressaoRegular/extraiEmail.py
+++ ExpressaoRegular/extraiEmail.py
@@ -1,12 +1,19 @@
 import re
 
-# ([aA-zZ]+|[0-9]+|^<).([aA-zZ]+|[0-9]+|.)@[^>,]+
+# Regex: \w+[\+\|.|\w]*@[.|\w]+
 def main():
     with open('e-mails.txt', 'r') as e:
-        # data = e.readlines()
-        for line in e:
-            print(line)
+        line = e.readlines()
+        listEmail = []
 
+        for pos in line:
+            email = re.findall(r"\w+[\+\|.|\w]*@[.|\w]+", pos)
+            listEmail.append(email[0]+ "\n")
+
+
+    with open('saida-email.txt', "w") as s:
+        [s.writelines(i) for i in listEmail]
+    
 if __name__ == "__main__":
     main()
 
