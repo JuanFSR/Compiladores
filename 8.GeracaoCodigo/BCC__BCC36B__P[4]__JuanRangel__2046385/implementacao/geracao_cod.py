c5883b75517ed070e395c65db141977dac041757
JuanFSR <juanfrangel15@gmail.com>
2022-06-02 09:52:11 -0300

    main: Iniciando geração de código e retirando bibliotecas inúteis da análise semântica

--- 8.GeracaoCodigo/BCC__BCC36B__P[4]__JuanRangel__2046385/implementacao/geracao_cod.py
+++ 8.GeracaoCodigo/BCC__BCC36B__P[4]__JuanRangel__2046385/implementacao/geracao_cod.py
@@ -2,10 +2,20 @@ import pandas as pd
 import tpp_semantic
 import numpy as np
 
+def gera_codigo(arvore_podada):
+    for no in arvore_podada.children:
+        print(no.label)
+
+        gera_codigo(no)
+
 def main():
     # Chamando análise semantica
-    tpp_semantic.main()
-    
+    arvore_podada = tpp_semantic.main()
+
+    print('------------------------------------')
+    print("Geração Código")
+
+    gera_codigo(arvore_podada)
 
 if __name__ == "__main__":
     main()
